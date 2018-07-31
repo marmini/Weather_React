9ec23d2fe4b169bdf99cb2d9bda6ccb2d2ca43b5
marmini <alluri.marmini@gmail.com>
2018-07-31 18:16:34 +0530

    WeatherReact

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
 import React, { Component } from 'react';
-import FirstComp from './FirstComp';
-import SecondComp from './SecondComp';
-
-var shapeData = ['Select Shape','square', 'circle', 'rectangle']
-var colorData = ['Select Color','red','blue','green'];
+import GeoLocation from './GeoLocation';
+import Weather from './Weather';
 
 export default class App extends Component {
 
   constructor(){
     super(); 
     this.state={
-     list:[],
+    
     }
-
-   this.listToPush=[]
-   this.appendToList = this.appendToList.bind(this);
-   this.clickedShape = this.clickedShape.bind(this);
   }
-
-  appendToList(shapeColor){
-   var booleanval=false;
-   if(this.state.list.length!==0){
-   for(var i=0;i<this.state.list.length;i++){
-   if(this.state.list[i].selectedShape===shapeColor.selectedShape && this.state.list[i].selectedColor===shapeColor.selectedColor){
-      shapeColor.selectedCount = this.state.list[i].selectedCount
-      booleanval=true;
-   }else{
-     if(booleanval===false){
-      shapeColor.selectedCount=0
-     }
-     
-   }
-   }
-  }
-   this.listToPush.push(shapeColor)
-   this.setState({list: this.listToPush});
-  }
-
-  clickedShape(state){
-    for(var i=0;i<this.state.list.length;i++){
-      if(state.shape===this.state.list[i].selectedShape && state.color===this.state.list[i].selectedColor){
-        this.state.list[i].selectedCount=state.count+1
-      }
-    }
-    this.forceUpdate()
-   }
 
   render() {
     return (
       <div>
-      <FirstComp shapes={shapeData} colors={colorData} getShapCol={this.appendToList}/>
-      <SecondComp listToRender={this.state.list} clickedShapeColor={this.clickedShape}/>
+         <GeoLocation/> 
+         <Weather />
       </div>
-    )
-}
+    )}
 }
---
+++ src/Weather.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import {Grid, Row} from 'react-bootstrap';
+import './Weather.css';
+
+const weatherApiKey='0a102e5fc588f4891af383b809a2ba86';
+var url = '';
+
+export default class Weather extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      latitude:0,
+      longitude:0,
+      temperature:0,
+      humidity:0,
+      country:'',
+      city:'',
+      icon:''
+    }
+  }
+  
+  setWeather=(position)=>{
+    this.lat=position.coords.latitude;
+    this.lon=position.coords.longitude;
+    
+    this.setState({
+      latitude:this.lat,
+      longitude:this.lon
+    });
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?'+'lat='+this.lat+'&lon='+this.lon+'&appid='+weatherApiKey;
+   console.log(url)
+    fetch(url)
+    .then(data => data.json())
+    .then(parsedData => this.setState({ 
+      country : parsedData.sys.country,
+      city :parsedData.name,
+      temperature : Number((parsedData.main.temp-273.15).toFixed(1)),
+      humidity : parsedData.main.humidity,
+      feelslike : parsedData.weather[0].description,
+      icon : parsedData.weather[0].icon
+    }));
+  
+  }
+  
+  getLocation=()=>{
+    navigator.geolocation.getCurrentPosition(this.setWeather);
+  }
+
+  componentWillMount(){
+    this.getLocation();
+  }
+
+  //render method
+  render() {
+    return (
+      <div id="appContainer">
+          <Grid>
+            <Row className="showGrid" id="imgRow">
+              <img src={'http://openweathermap.org/img/w/'+this.state.icon+'.png'} alt="" id="weatherImage" />
+            </Row>
+            <Row className="showGrid" id="tempRow">
+              <label className="bigText">{this.state.temperature}ºC</label>
+            </Row>
+            <Row className="showGrid" id="humidityRow">
+              <label className="regularText">Humidity:</label>
+              <label className="regularText">{this.state.humidity}%</label>
+            </Row> 
+            <Row className="showGrid" id="locationRow">
+              <label className="regularText">{this.state.city},</label>
+              <label className="regularText"> {this.state.country}</label>
+            </Row>
+          </Grid>
+      </div>
+    )
+  }
+}
